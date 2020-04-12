25137eb70bc76a8dc2e2901e4f2b097ff7928bed
Abdirisak Abdalla <xijaas100@gmail.com>
2020-04-12 04:24:39 -0700

    dfdfdf

--- DemoProject/src/com/jacfu/beans/utility.java
+++ DemoProject/src/com/jacfu/beans/utility.java
@@ -4,6 +4,7 @@ public class utility {
 	public static void main(String[]args){
 		System.out.println("hello jacfuu");
 		System.out.print("hello world abdfdfdfdf");
+		System.out.print("hello worldmmofdfddfdfdfdfd");
 	}
 
 }

798f6af1683f259d79272f790f58d8f9d2988b1e
Abdirisak Abdalla <xijaas100@gmail.com>
2020-04-12 04:17:01 -0700

    dev1 modified

--- DemoProject/src/com/jacfu/test/test.java
+++ DemoProject/src/com/jacfu/test/test.java
@@ -3,7 +3,8 @@ package com.jacfu.test;
 public class test {
 	public static void main(String args[]){
 		System.out.print("hello world");
-		System.out.print("come change");
+		System.out.print("Tl come change");
+		System.out.print(" dev1 hello world");
 	}
 	
 
